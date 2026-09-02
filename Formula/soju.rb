3c649c63a9cf734a0ff9d803f981f94277a502bd
Mike Kim <mikekim@Mikes-Mac-mini.local>
2026-09-02 14:42:19 -0700

    soju 1.3.1

--- Formula/soju.rb
+++ Formula/soju.rb
@@ -1,10 +1,10 @@
 class Soju < Formula
   desc "Free Battle.net, Steam, Epic and GOG launchers on Apple Silicon"
   homepage "https://github.com/BCD1210/soju"
-  url "https://github.com/BCD1210/soju/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "16f064eb0aac964214309b45d1961169cc3e608517b764830b605a57c4457e07"
+  url "https://github.com/BCD1210/soju/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "66564da557c48ebc80c42f535bf07210d0b1068ae621ef4339f629c84886e649"
   license "GPL-3.0-or-later"
-  version "1.3.0"
+  version "1.3.1"
 
   depends_on :macos
   depends_on arch: :arm64
